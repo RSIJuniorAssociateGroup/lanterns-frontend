c330652a8aeaf59afba43f3371c7ca8e997debce
Brandon Huffman <bt_huffman@msn.com>
2019-10-09 17:56:40 -0600

    Created a basic test for using RotateTileRight. Includes commented out options that also can work with adjustment to the list = prop to being list = props.list

--- lanterns/src/App.js
+++ lanterns/src/App.js
@@ -1,26 +1,75 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function RotateTileRight(props) {
+  console.log("Right");
+  let item = "";
+  let list = props;
+  item = list[3];
+  list.pop();
+  list.unshift(item);
+  console.log(list);
+  return list;
+}
+
+function RotateTileLeft(list) {
+  console.log("Left");
+  let item = "";
+  item = list[0];
+  list.shift();
+  list.unshift(item);
+  console.log(list);
+  return list;
+}
+
+function TestRotationRightOnce() {
+  console.log("This is the begining of the lake orientation test. First we create a base lakeTile with the same object structure as that found in the c#.")
+
+  let lakeTile00 = {};
+  lakeTile00.id = 0;
+  lakeTile00.colorList = ["red", "white", "blue", "green"];
+
+  console.log(lakeTile00);
+
+  console.log("Then we rotate the tile right.");
+  console.log(RotateTileRight(lakeTile00.colorList));
+  return lakeTile00;
+}
+
+// function TestRoration() {
+//   return (
+//     <button onClick={() => TestRotationRightOnce()}>
+//       Run test to rotate the tile right.
+//   </button>
+//   );
+// }
+
+function Welcome(props) {
+  return <h1>Hello, {props.name}</h1>;
+  TestRotationRightOnce();
+}
+
+let tile01 = {
+  id: 1,
+  colorList: ["yellow", "gold", "indigo", "turqouise"],
+}
+
+class App extends React.Component {
+  render() 
+  {
+    // TestRotationRightOnce();
+    return (
+      <div className="App">
+        <div className="App-body">
+          <Welcome name="Sara" />
+          {/* <RotateTileRight list= {["red", "white", "blue", "green"]} /> */}
+          <button onClick = {() => RotateTileRight(tile01.colorList)}>
+
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;

9e703075303f364723bf1e7aad97f03bd63da2e1
Brandon Huffman <bt_huffman@msn.com>
2019-10-10 10:32:17 -0600

    Created a map function to create 4 players and edited constructor.

--- lanterns/src/App.js
+++ lanterns/src/App.js
@@ -5,24 +5,26 @@ class Player extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      playerId: 0,
-      playerName: this.props,
-      lakeTileHand: [],
-      playerHonorScore: 0,
+      playerId: this.props.playerId,
+      playerName: this.props.playerName,
+      lakeTileHand: this.props.lakeTileHand,
+      playerHonorScore: this.props.playerHonorScore,
       playerActive: false
     }
   }
-    render() {
-      console.log(this.props.playerId);
-      return (
-      <h1>Player properties: </h1>
-      // <h2>{this.props.playerId}</h2>,
-      // <h2>{this.props.playerName}</h2>,
-      // <h2>{this.props.children}</h2>,
-      // <h2>{this.props.playerHonorScore}</h2>,
-      // <h2>{this.props.playerActive}</h2>
-      );
-    }
+  render() {
+    console.log(this.state.playerId);
+    return (
+      <div>
+        <h1>Player properties: </h1>
+        <p>{this.state.playerId}</p>
+        <p>{this.state.playerName}</p>
+        <p>{this.state.lakeTileHand}</p>
+        <p>{this.state.playerHonorScore}</p>
+        <p>{this.state.playerActive}</p>
+      </div>
+    );
+  }
 
 }
 
@@ -30,35 +32,23 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Player 
-        playerId={0} 
-        playerName="Brandon" 
-        lakeTileHand={[1, 4, 7, 36]} 
-        // playerHonorScore={0} 
-        // playerActive={false}
-        />
+        {[0, 1, 2, 3].map((i) => {
+          return (
+            <Player
+              playerId={i}
+              playerName="Brandon"
+              lakeTileHand={[1, 4, 7, 36]}
+              playerHonorScore={0}
+              playerActive="false"
+            />
+          );
+        }
+        )
+    }
       </header>
     </div>
   );
 }
 
-/*
-class App extends React.Component {
-  constructor(props){
-    super(props);
-    this.handleEvent = this.handleEvent.bind(this);
-  }
-  handleEvent(){
-    console.log(this.props);
-  }
-  render() {
-    return (
-      <div className="App">
-        <button onClick={this.handleEvent}>Please Click</button>
-      </div>
-    );
-  }
-}
-*/
 export default App;
 

06130e4b7257ce45a8b5e304047c103d3277d7f7
Brandon Huffman <bt_huffman@msn.com>
2019-10-10 09:49:30 -0600

    Built the constructor for the player class and working to get it to show up on browser.

--- lanterns/src/App.js
+++ lanterns/src/App.js
@@ -1,26 +1,64 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+class Player extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      playerId: 0,
+      playerName: this.props,
+      lakeTileHand: [],
+      playerHonorScore: 0,
+      playerActive: false
+    }
+  }
+    render() {
+      console.log(this.props.playerId);
+      return (
+      <h1>Player properties: </h1>
+      // <h2>{this.props.playerId}</h2>,
+      // <h2>{this.props.playerName}</h2>,
+      // <h2>{this.props.children}</h2>,
+      // <h2>{this.props.playerHonorScore}</h2>,
+      // <h2>{this.props.playerActive}</h2>
+      );
+    }
+
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Player 
+        playerId={0} 
+        playerName="Brandon" 
+        lakeTileHand={[1, 4, 7, 36]} 
+        // playerHonorScore={0} 
+        // playerActive={false}
+        />
       </header>
     </div>
   );
 }
 
+/*
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.handleEvent = this.handleEvent.bind(this);
+  }
+  handleEvent(){
+    console.log(this.props);
+  }
+  render() {
+    return (
+      <div className="App">
+        <button onClick={this.handleEvent}>Please Click</button>
+      </div>
+    );
+  }
+}
+*/
 export default App;
+

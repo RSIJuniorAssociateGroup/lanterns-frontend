fce696de215b8d75d2f3496e8f27425113b6b73e
Brandon Huffman <bt_huffman@msn.com>
2019-10-09 18:08:20 -0600

    Completed the unit testing and changed some text.

--- lanterns/src/App.js
+++ lanterns/src/App.js
@@ -9,62 +9,44 @@ function RotateTileRight(props) {
   list.pop();
   list.unshift(item);
   console.log(list);
-  return list;
+  return (
+    <div>
+    {tile01.colorList}
+  </div>
+  );
 }
 
-function RotateTileLeft(list) {
+function RotateTileLeft(props) {
   console.log("Left");
   let item = "";
+  let list = props;
   item = list[0];
   list.shift();
-  list.unshift(item);
+  list.push(item);
   console.log(list);
   return list;
 }
 
-function TestRotationRightOnce() {
-  console.log("This is the begining of the lake orientation test. First we create a base lakeTile with the same object structure as that found in the c#.")
-
-  let lakeTile00 = {};
-  lakeTile00.id = 0;
-  lakeTile00.colorList = ["red", "white", "blue", "green"];
-
-  console.log(lakeTile00);
-
-  console.log("Then we rotate the tile right.");
-  console.log(RotateTileRight(lakeTile00.colorList));
-  return lakeTile00;
-}
-
-// function TestRoration() {
-//   return (
-//     <button onClick={() => TestRotationRightOnce()}>
-//       Run test to rotate the tile right.
-//   </button>
-//   );
-// }
-
-function Welcome(props) {
-  return <h1>Hello, {props.name}</h1>;
-  TestRotationRightOnce();
-}
-
+//Test data to be passed for Rotating the Tile
 let tile01 = {
   id: 1,
-  colorList: ["yellow", "gold", "indigo", "turqouise"],
+  colorList: ["yellow ", "gold ", "indigo ", "turqouise "],
 }
 
 class App extends React.Component {
   render() 
   {
-    // TestRotationRightOnce();
     return (
       <div className="App">
         <div className="App-body">
-          <Welcome name="Sara" />
-          {/* <RotateTileRight list= {["red", "white", "blue", "green"]} /> */}
           <button onClick = {() => RotateTileRight(tile01.colorList)}>
-
+            Rotate Sample Tile Right
+          </button>
+          <div>
+            {/* {tile01.colorList} */}
+          </div>
+          <button onClick = {() => RotateTileLeft(tile01.colorList)}>
+            Rotate Sample Tile Left
           </button>
         </div>
       </div>

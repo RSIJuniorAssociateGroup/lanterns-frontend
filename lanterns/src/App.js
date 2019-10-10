7e4c990d0a8c26975da727197d2bc390dc8c0153
Brandon Huffman <bt_huffman@msn.com>
2019-10-10 13:04:13 -0600

    Added the playerSeats array and unit test for it.

--- lanterns/src/App.js
+++ lanterns/src/App.js
@@ -1,55 +1,8 @@
 import React from 'react';
 import './App.css';
 
-<<<<<<< HEAD
-function RotateTileRight(props) {
-  console.log("Right");
-  let item = "";
-  let list = props;
-  item = list[list.length -1];
-  list.pop();
-  list.unshift(item);
-  console.log(list);
-  return list;
-}
+const playerSeats = []
 
-function RotateTileLeft(props) {
-  console.log("Left");
-  let item = "";
-  let list = props;
-  item = list[0];
-  list.shift();
-  list.push(item);
-  console.log(list);
-  return list;
-}
-
-//Test data to be passed for Rotating the Tile
-let tile01 = {
-  id: 1,
-  colorList: ["yellow ", "gold ", "indigo ", "turqouise "],
-}
-
-class App extends React.Component {
-  render() 
-  {
-    return (
-      <div className="App">
-        <div className="App-body">
-          <button onClick = {() => RotateTileRight(tile01.colorList)}>
-            Rotate Sample Tile Right
-          </button>
-          <div>
-            {/* {tile01.colorList} */}
-          </div>
-          <button onClick = {() => RotateTileLeft(tile01.colorList)}>
-            Rotate Sample Tile Left
-          </button>
-        </div>
-      </div>
-    );
-  }
-=======
 class Player extends React.Component {
   constructor(props) {
     super(props);
@@ -62,15 +15,18 @@ class Player extends React.Component {
     }
   }
   render() {
-    console.log(this.state.playerId);
+    console.log("This player's id is " + this.state.playerId);
+    console.log("This player's lake tile hand by LakeTile id is " + this.state.lakeTileHand);
+    console.log("This Player's honor score total is" + this.state.playerHonorScore);
+    console.log("This player's active status is " + this.state.playerActive);   
+
+    playerSeats.push(this.state.playerId); 
+    console.log("Players are seated top to bottom by id as follows: " + playerSeats);
     return (
       <div>
         <h1>Player properties: </h1>
         <p>{this.state.playerId}</p>
         <p>{this.state.playerName}</p>
-        <p>{this.state.lakeTileHand}</p>
-        <p>{this.state.playerHonorScore}</p>
-        <p>{this.state.playerActive}</p>
       </div>
     );
   }
@@ -78,26 +34,25 @@ class Player extends React.Component {
 }
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        {[0, 1, 2, 3].map((i) => {
-          return (
-            <Player
-              playerId={i}
-              playerName="Brandon"
-              lakeTileHand={[1, 4, 7, 36]}
-              playerHonorScore={0}
-              playerActive="false"
-            />
-          );
-        }
-        )
-    }
-      </header>
-    </div>
-  );
->>>>>>> 9e703075303f364723bf1e7aad97f03bd63da2e1
+    return (
+      <div className="App">
+        <header className="App-header">
+          {[0, 1, 2, 3].map((i) => {
+            return (
+              <Player
+                playerId={i}
+                playerName="Brandon"
+                lakeTileHand={[4, 7, 36]}
+                playerHonorScore={0}
+                playerActive="false"
+              />
+            );
+          }
+          )
+          }
+        </header>
+      </div>
+    );
 }
 
 export default App;

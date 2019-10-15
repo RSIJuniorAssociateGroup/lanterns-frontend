1ae68f95121b0c8a0c032ce8157d14f75acb81fc
Brandon Huffman <bt_huffman@msn.com>
2019-10-15 09:17:16 -0600

    Changed the App function to a class react component.

--- lanterns/src/App.js
+++ lanterns/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import { REFUSED } from 'dns';
 
 class Player extends React.Component {
   constructor(props) {
@@ -20,35 +21,35 @@ class Player extends React.Component {
         <h1>Player properties: </h1>
         <p>{this.state.playerId}</p>
         <p>{this.state.playerName}</p>
-        <p>{this.state.lakeTileHand}</p>
         <p>{this.state.playerHonorScore}</p>
-        <p>{this.state.playerActive}</p>
       </div>
     );
   }
 
 }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        {[0, 1, 2, 3].map((i) => {
-          return (
-            <Player
-              playerId={i}
-              playerName="Brandon"
-              lakeTileHand={[1, 4, 7, 36]}
-              playerHonorScore={0}
-              playerActive="false"
-            />
-          );
-        }
-        )
-    }
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          {[0, 1, 2, 3].map((i) => {
+            return (
+              <Player
+                playerId={i}
+                playerName="Brandon"
+                lakeTileHand={[1, 4, 7, 36]}
+                playerHonorScore={0}
+                playerActive="false"
+              />
+            );
+          }
+          )
+      }
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;

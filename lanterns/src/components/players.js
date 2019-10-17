5551c319afabede6720d7c73858314fad99ce956
Brandon Huffman <bt_huffman@msn.com>
2019-10-17 15:20:34 -0600

    Updated file directory structure and seperate everything except for the board into seperate components. Created the board based off of the game of life board.

---
+++ lanterns/src/components/players.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+// PLAYER 
+const playerSeats = [];
+
+class Player extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            playerId: this.props.playerId,
+            playerName: this.props.playerName,
+            lakeTileHand: this.props.lakeTileHand,
+            playerHonorScore: this.props.playerHonorScore,
+            playerActive: false
+        }
+    }
+    render() {
+        console.log("This player's id is " + this.state.playerId);
+        console.log("This player's name is " + this.state.playerName);
+        console.log("This player's lake tile hand by LakeTile id is " + this.state.lakeTileHand);
+        console.log("This Player's honor score total is" + this.state.playerHonorScore);
+        console.log("This player's active status is " + this.state.playerActive);
+
+        playerSeats.push(this.state.playerId);
+        console.log("Players are seated top to bottom by id as follows: " + playerSeats);
+        return (
+            <div className="player">
+                <p>{this.state.playerName}</p>
+            </div>
+        );
+    }
+}
+
+export default Player; 
---
+++ lanterns/src/tests/board.test.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+let mockTiles = {
+// Mock tiles based on actual GameData lake_tiles_list.csv
+tile07: {
+    TileId: 7,
+    colorList: [" yellow", " white", " green", " red"]
+  },
+tile10: {
+    TileId: 10,
+    colorList: [" purple", " blue", " blue", " black"]
+  },
+tile11: {
+    TileId: 11,
+    colorList: [" red", " blue", " white", " yellow"]
+  },
+tile22: {
+    TileId: 22,
+    colorList: [" white", " black", " yellow", " black"]
+  },
+tile30: {
+    TileId: 30,
+    colorList: [" yellow", " green", " white", " green"]
+  }
+}
+
+export default mockTiles;

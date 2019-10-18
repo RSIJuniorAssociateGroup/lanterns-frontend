05586d93c584b9a8b78270d1668b94a3c47aad61
matthew.morgan <matthew.morgan@ruralsourcing.com>
2019-10-18 10:49:23 -0600

    Added basic component for the PlayerHand.

---
+++ lanterns/src/PlayerHand.js
@@ -0,0 +1,23 @@
+import React from "react"
+import "./PlayerHand.css"
+import tile from "./tile02.png"
+
+class PlayerHand extends React.Component {
+    render() {
+        return (
+            <div className="player-hand" >
+                <div className="lake-tile-holder">
+                    <img src={tile} alt="Something" />
+                </div>
+                <div className="lake-tile-holder">
+                    <img src={tile} alt="Something" />
+                </div>
+                <div className="lake-tile-holder">
+                    <img src={tile} alt="Something" />
+                </div>
+            </div>
+        );
+    }
+}
+
+export default PlayerHand

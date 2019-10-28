8fa79510658baedd767135d123c016987ab90fbe
matthew.morgan <matthew.morgan@ruralsourcing.com>
2019-10-28 13:59:52 -0600

    Added logic to make LakeTiles programmatically be in player's hand.

--- lanterns/src/components/Player/PlayerLakeTiles.js
+++ lanterns/src/components/Player/PlayerLakeTiles.js
@@ -4,42 +4,51 @@ import LakeTile from "../LakeTileComponent/LakeTile";
 import BoardTile from "../Board/BoardTile";
 
 class PlayerLakeTiles extends React.Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            playerHand: this.makeHand()
+        }
+
+    }
+
+    makeHand() {
+        let tempPlayerHand = [
+            <LakeTile
+                id="lakeTile-10"
+                draggable="true"
+                topColor={2}
+                rightColor={4}
+                bottomColor={5}
+                leftColor={3}
+            />,
+            <LakeTile
+                id="lakeTile-11"
+                draggable="true"
+                topColor={3}
+                rightColor={4}
+                bottomColor={1}
+                leftColor={2}
+            />,
+            <LakeTile
+                id="lakeTile-12"
+                draggable="true"
+                topColor={1}
+                rightColor={6}
+                bottomColor={7}
+                leftColor={5}
+            />
+        ]
+
+        return tempPlayerHand
+    }
+
     render() {
         return (
             <div className="player-hand" >
-                <div className="lake-tile-holder">
-                    <LakeTile
-                        id="lakeTile-10"
-                        draggable="true"
-                        topColor={2}
-                        rightColor={4}
-                        bottomColor={5}
-                        leftColor={3}
-                    >
-                    </LakeTile>
-                </div>
-                <div className="lake-tile-holder">
-                    <LakeTile
-                        id="lakeTile-11" 
-                        draggable="true"
-                        topColor={2}
-                        rightColor={4}
-                        bottomColor={5}
-                        leftColor={3}
-                    >
-                    </LakeTile>
-                </div>
-                <div className="lake-tile-holder">
-                    <LakeTile
-                        id="lakeTile-12" 
-                        draggable="true"
-                        topColor={2}
-                        rightColor={4}
-                        bottomColor={5}
-                        leftColor={3}
-                    >
-                    </LakeTile>
-                </div>
+                {this.state.playerHand.map(tile => <div className="lake-tile-holder">{tile}</div>)}
             </div>
         );
     }

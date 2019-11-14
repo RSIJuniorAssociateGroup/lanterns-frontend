39438165d4a29164249de53abf61956b8ba50c12
Brandon Huffman <bt_huffman@msn.com>
2019-11-14 08:41:51 -0700

    Have function and class ready for lake tile, niether one works.

--- lanterns/src/components/LakeTileComponent/LakeTile.js
+++ lanterns/src/components/LakeTileComponent/LakeTile.js
@@ -1,49 +1,73 @@
 import React from 'react';
 import './LakeTile.css';
 
-function LakeTile(props) {
-    // class LakeTile extends React.Component {
-    // constructor(props) {
-    //     super(props)
+// function LakeTile(props) {
+class LakeTile extends React.Component {
+    constructor(props) {
+        super(props)
 
-    //     // this.getLanternImage = this.props.getLanternImage.bind(this);
-    //     this.state = {
-    //         // colors: props.colors
-    //     }
-    // }
+        this.getLanternImage = this.props.getLanternImage.bind(this);
+        // this.state = {
+            // colors: props.colors
+        // }
+    }
 
-    // render() {
-    // const top = this.getLanternImage(this.state.colors[0])
-    // const right = this.getLanternImage(this.state.colors[1])
-    // const bottom = this.getLanternImage(this.state.colors[2])
-    // const left = this.getLanternImage(this.state.colors[3])
-    return (
-        <div>
-            {props.children}
-            <div
-                id={props.id}
-                className="parentContainer"
-                draggable={props.draggable}
-                onDragStart={props.dragStart}
-                onDragOver={props.dragOver}
-                colors={props.colors}
-                rotate={props.canRotate}
-            >
-                <div className="parent"
-                    onClick={props.rotate}>
-                    <div className="topTri" style={{ backgroundImage: "url(" + props.top + ")" }}>
-                    </div>
-                    <div className="rightTri" style={{ backgroundImage: "url(" + props.right + ")" }}>
-                    </div>
-                    <div className="leftTri" style={{ backgroundImage: "url(" + props.left + ")" }}>
-                    </div>
-                    <div className="bottomTri" style={{ backgroundImage: "url(" + props.bottom + ")" }}>
+    render() {
+        // const top = this.getLanternImage(this.state.colors[0])
+        // const right = this.getLanternImage(this.state.colors[1])
+        // const bottom = this.getLanternImage(this.state.colors[2])
+        // const left = this.getLanternImage(this.state.colors[3])
+        return (
+            // <div>
+            //     {props.children}
+            //     <div
+            //         id={props.id}
+            //         className="parentContainer"
+            //         draggable={props.draggable}
+            //         onDragStart={props.dragStart}
+            //         onDragOver={props.dragOver}
+            //         colors={props.colors}
+            //         rotate={props.canRotate}
+            //     >
+            //         <div className="parent"
+            //             onClick={props.rotate}>
+            //             <div className="topTri" style={{ backgroundImage: "url(" + props.getLanternImage(this.state.colors[0]) + ")" }}>
+            //             </div>
+            //             <div className="rightTri" style={{ backgroundImage: "url(" + props.getLanternImage(this.state.colors[1]) + ")" }}>
+            //             </div>
+            //             <div className="leftTri" style={{ backgroundImage: "url(" + props.getLanternImage(this.state.colors[2]) + ")" }}>
+            //             </div>
+            //             <div className="bottomTri" style={{ backgroundImage: "url(" + props.getLanternImage(this.state.colors[3]) + ")" }}>
+            //             </div>
+            //         </div>
+            //     </div>
+            // </div>
+            <div>
+                {this.props.children}
+                <div
+                    id={this.props.id}
+                    className="parentContainer"
+                    draggable={this.props.draggable}
+                    onDragStart={this.props.dragStart}
+                    onDragOver={this.props.dragOver}
+                    colors={this.props.colors}
+                    rotate={this.props.canRotate}
+                >
+                    <div className="parent"
+                        onClick={this.props.rotate}>
+                        <div className="topTri" style={{ backgroundImage: "url(" + this.getLanternImage(this.state.colors[0]) + ")" }}>
+                        </div>
+                        <div className="rightTri" style={{ backgroundImage: "url(" + this.getLanternImage(this.state.colors[1]) + ")" }}>
+                        </div>
+                        <div className="leftTri" style={{ backgroundImage: "url(" + this.getLanternImage(this.state.colors[2]) + ")" }}>
+                        </div>
+                        <div className="bottomTri" style={{ backgroundImage: "url(" + this.getLanternImage(this.state.colors[3]) + ")" }}>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
     )
+    }
 }
-// }
 
-export default LakeTile
+export default LakeTile;
